3378bc60c076a9b37f2604ecb94d05eb1e138928
Nikiforov Konstantin <kostya.nik.3854@gmail.com>
2023-06-25 15:02:41 +0500

    fix for deploy 3

--- deploy.sh
+++ deploy.sh
@@ -16,14 +16,19 @@ server {
 
     server_name 158.160.48.156;
 
-    location / {
+    location /api/register {
         proxy_pass                          http://127.0.0.1:8080;
         proxy_set_header                    Host \$host;
         proxy_set_header X-Forwarded-Host   \$server_name;
         proxy_set_header X-Real-IP          \$remote_addr;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection \$connection_upgrade;
+
+    }
+
+        location /api/game {
+        proxy_pass                          ws://127.0.0.1:8080;
+        proxy_set_header                    Host \$host;
+        proxy_set_header X-Forwarded-Host   \$server_name;
+        proxy_set_header X-Real-IP          \$remote_addr;
 
     }
 
